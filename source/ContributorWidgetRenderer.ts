193aac559a8549f69172466555076dd11b770c63
Henry Hirsch <henry@w3-net.de>
2017-12-09 17:43:40 +0100

    Fix indentation in renderer

--- source/ContributorWidgetRenderer.ts
+++ source/ContributorWidgetRenderer.ts
@@ -12,22 +12,22 @@ export class ContributorWidgetRenderer {
 
     render(targetDiv: any, contributors: ContributorJson[]): void {
         if (targetDiv != null) {
-        contributors.sort(sortByContributions);
-        var blockedUsers = new Array("gitter-badger");
+            contributors.sort(sortByContributions);
+            var blockedUsers = new Array("gitter-badger");
             for(var n=0;n<contributors.length;n++){
                 var contributor: Contributor;
                 contributor = new Contributor(contributors[n])
-            if(!contributor.isOnList(blockedUsers)) {
-                var contributorDiv = document.createElement('div');
-                var contributorHtml = '<a href=\"' + contributor.getProfileUrl() + '\" target=\"_blank\">';
-                contributorHtml += contributor.getName();
-                contributorHtml += '</a></br>';
-                contributorDiv.innerHTML = contributorHtml;
-                targetDiv.appendChild(contributorDiv);
+                if(!contributor.isOnList(blockedUsers)) {
+                    var contributorDiv = document.createElement('div');
+                    var contributorHtml = '<a href=\"' + contributor.getProfileUrl() + '\" target=\"_blank\">';
+                    contributorHtml += contributor.getName();
+                    contributorHtml += '</a></br>';
+                    contributorDiv.innerHTML = contributorHtml;
+                    targetDiv.appendChild(contributorDiv);
+                }
             }
+        } else {
+            console.log('Target div not found.');
         }
-    } else {
-        console.log('Target div not found.');
-    }
     }
 }
