9b985fb1c3218370c2c589e5d11e829e539b0225
Henry Hirsch <henry@w3-net.de>
2017-12-09 17:56:21 +0100

    Add service to generate html

--- source/ContributorWidgetRenderer.ts
+++ source/ContributorWidgetRenderer.ts
@@ -2,6 +2,7 @@ import { getJsonFromUrl } from "./Json";
 import { getJsonCallback } from "./Json";
 import { ContributorJson } from "./ContributorJson";
 import { Contributor } from "./Contributor";
+import { HtmlService } from "./HtmlService";
 
 function sortByContributions(x: ContributorJson, y: ContributorJson) {
   return  y.contributions - x.contributions;
@@ -11,17 +12,18 @@ export class ContributorWidgetRenderer {
     contributorsJson: ContributorJson[];
 
     render(targetDiv: any, contributors: ContributorJson[]): void {
+        var htmlService: HtmlService;
+        var blockedUsers = new Array("gitter-badger");
+        htmlService = new HtmlService();
         if (targetDiv != null) {
             contributors.sort(sortByContributions);
-            var blockedUsers = new Array("gitter-badger");
             for(var n=0;n<contributors.length;n++){
-                var contributor: Contributor;
-                contributor = new Contributor(contributors[n])
+                var contributor: Contributor = new Contributor(contributors[n])
                 if(!contributor.isOnList(blockedUsers)) {
                     var contributorDiv = document.createElement('div');
-                    var contributorHtml = '<a href=\"' + contributor.getProfileUrl() + '\" target=\"_blank\">';
-                    contributorHtml += contributor.getName();
-                    contributorHtml += '</a></br>';
+                    var contributorHtml =
+                        htmlService.getAnchorHtml(contributor.getProfileUrl(), contributor.getName());
+                    contributorHtml += '</br>';
                     contributorDiv.innerHTML = contributorHtml;
                     targetDiv.appendChild(contributorDiv);
                 }
